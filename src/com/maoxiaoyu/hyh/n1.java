427c28c69438dc1fd0a446ff867caee578f586f5
猫小鱼hyh~ <2714690577@qq.com>
2023-05-12 23:23:19 +0800

    123

--- src/com/maoxiaoyu/hyh/n1.java
+++ src/com/maoxiaoyu/hyh/n1.java
@@ -7,5 +7,7 @@ package com.maoxiaoyu.hyh;
 public class n1 {
     public static void main(String[] args) {
         System.out.println("123");
+        System.out.println("123456");
+        System.out.println("gitee");
     }
 }
